2d9e8fa82ff05e5c80aea56bd888c1707dd104a8
unknown <mahmodulshahin665@gmail.com>
2022-04-01 21:00:26 +0600

    handle two btn calc

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
-document.getElementById('eightGB').addEventListener('click', function () {
-    alert('is add')
-});
+// document.getElementById('eightGB').addEventListener('click', function () {
+//     alert('is add')
+// });
 document.getElementById('sixteenGB').addEventListener('click', function () {
-    alert('is add too')
+    // const memoryCost = document.getElementById('memory-cost');
+    // memoryCost.innerText = 200;
+
+    // const bestPrice = document.getElementById('best-price').innerText;
+    // const memoryPrice = document.getElementById('memory-cost').innerText;
+    // const storagePrice = document.getElementById('storage-cost').innerText;
+    // const deliveryPrice = document.getElementById('delivery-cost').innerText;
+
+    // const totalPrice = parseFloat(bestPrice) + parseFloat(memoryPrice) + parseFloat(storagePrice) + parseFloat(deliveryPrice);
+
+    // const total = document.getElementById('total-price');
+    // total.innerText = totalPrice;
+    updatePrice('memory-cost', 200)
 });
+document.getElementById('ssd3').addEventListener('click', function () {
+    updatePrice('storage-cost', 300);
+})
+function updatePrice(itemID, price) {
+    const memoryCost = document.getElementById(itemID);
+    memoryCost.innerText = price;
+
+    const bestPrice = document.getElementById('best-price').innerText;
+    const memoryPrice = document.getElementById('memory-cost').innerText;
+    const storagePrice = document.getElementById('storage-cost').innerText;
+    const deliveryPrice = document.getElementById('delivery-cost').innerText;
+
+    const totalPrice = parseFloat(bestPrice) + parseFloat(memoryPrice) + parseFloat(storagePrice) + parseFloat(deliveryPrice);
+
+    const total = document.getElementById('total-price');
+    total.innerText = totalPrice;
+}
