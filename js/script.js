a16266c6b34c375bb4aeab2b334cab9813f511a8
unknown <mahmodulshahin665@gmail.com>
2022-04-01 16:21:06 +0600

    js file added

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('eightGB').addEventListener('click', function () {
+    alert('is add')
+});
+document.getElementById('sixteenGB').addEventListener('click', function () {
+    alert('is add too')
+});
