f95cf92002aef1aa9d034f68e19a00ee6cd2fdfe
unknown <mahmodulshahin665@gmail.com>
2022-04-02 11:54:49 +0600

    add promo code and error handler

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
+//get innertext and update total price;
 function updatePrice(itemID, price) {
     const memoryCost = document.getElementById(itemID);
     memoryCost.innerText = price;
-
     const bestPrice = document.getElementById('best-price').innerText;
     const memoryPrice = document.getElementById('memory-cost').innerText;
     const storagePrice = document.getElementById('storage-cost').innerText;
     const deliveryPrice = document.getElementById('delivery-cost').innerText;
-
     const totalPrice = parseFloat(bestPrice) + parseFloat(memoryPrice) + parseFloat(storagePrice) + parseFloat(deliveryPrice);
 
     const total = document.getElementById('total-price');
     total.innerText = totalPrice;
 }
 
+//handle the right btn and set price;
 function onclick(clickId, updateId, price) {
     document.getElementById(clickId).addEventListener('click', function () {
         updatePrice(updateId, price);
@@ -26,4 +26,33 @@ onclick('ssd3', 'storage-cost', 300);
 onclick('free-delivery', 'delivery-cost', 0);
 onclick('paid-delivery', 'delivery-cost', 20);
 
+//promo code section
+const fakeCode = 'pHero'
+document.getElementById('apply-btn').addEventListener('click', function () {
+    const userCode = document.getElementById('promo-input').value;
+
+    const rightPromoCode = document.getElementById('applied-text');
+    const wrongPromoCode = document.getElementById('not-applied-text')
+
+    if (userCode == fakeCode) {
+        const total = document.getElementById('total-price');
+        let totalPrice = parseFloat(total.innerText);
+        //20% discount
+        const discountPrice = totalPrice * .2;
+        totalPrice = totalPrice - discountPrice;
+        //update price
+        total.innerText = totalPrice;
+
+        //update message
+        rightPromoCode.style.display = 'block';
+        wrongPromoCode.style.display = 'none'
+
+
+    }
+    else {
+        wrongPromoCode.style.display = 'block';
+        rightPromoCode.style.display = 'none';
+    }
+
+})
 
